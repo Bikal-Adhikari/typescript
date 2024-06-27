96bfdd184253641e13400d5e6f7bc50f64b1eaad
Bikal <bikaladhikari5@gmail.com>
2024-06-27 19:31:10 +1000

    suprise calc

---
+++ supriseCalculator.ts
@@ -0,0 +1,18 @@
+interface Calculator {
+  add: (a: number, b: number) => number;
+  subtract: (a: number, b: number) => number;
+  multiply: (a: number, b: number) => number;
+  divide: (a: number, b: number) => number;
+  surprise: () => string;
+}
+
+const calculator: Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+  surprise: () => "BOOM! You didn't expect that, did you?",
+};
+
+console.log(calculator.add(2, 3));
+console.log(calculator.surprise());
