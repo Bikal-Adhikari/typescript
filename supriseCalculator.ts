47463115f446b95b333298f16f5ed4296bf0ca03
Bikal <bikaladhikari5@gmail.com>
2024-06-27 19:36:44 +1000

    Simple math operations

--- supriseCalculator.ts
+++ supriseCalculator.ts
@@ -16,3 +16,61 @@ const calculator: Calculator = {
 
 console.log(calculator.add(2, 3));
 console.log(calculator.surprise());
+
+class Maths {
+  private num1: number;
+  private num2: number;
+  private operator: string;
+
+  constructor() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.operator = "";
+  }
+
+  public setNum1(num: number): void {
+    this.num1 = num;
+  }
+
+  public setNum2(num: number): void {
+    this.num2 = num;
+  }
+
+  public setOperator(operator: string): void {
+    this.operator = operator;
+  }
+
+  public calculate(): number {
+    switch (this.operator) {
+      case "+":
+        return this.num1 + this.num2;
+      case "-":
+        return this.num1 - this.num2;
+      case "*":
+        return this.num1 * this.num2;
+      case "/":
+        if (this.num2 === 0) {
+          throw new Error("Cannot divide by zero!");
+        }
+        return this.num1 / this.num2;
+      default:
+        throw new Error("Invalid operator!");
+    }
+  }
+}
+
+// usage
+const math = new Maths();
+math.setNum1(10);
+math.setNum2(2);
+math.setOperator("+");
+console.log(math.calculate());
+
+math.setOperator("-");
+console.log(math.calculate());
+
+math.setOperator("*");
+console.log(math.calculate());
+
+math.setOperator("/");
+console.log(math.calculate());
